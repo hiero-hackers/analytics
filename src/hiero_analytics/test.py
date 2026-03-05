64bccc9e257f7c3558208a58b95b88b979da1fcb
exploreriii <133720349+exploreriii@users.noreply.github.com>
2026-03-05 23:23:51 +0000

    feat: rudimentary fetch
    Signed-off-by: exploreriii <133720349+exploreriii@users.noreply.github.com>

---
+++ src/hiero_analytics/test.py
@@ -0,0 +1,12 @@
+from hiero_analytics.data_sources.github_client import GitHubClient
+from hiero_analytics.data_sources.github_graphql import fetch_repo_issues_graphql
+
+client = GitHubClient()
+
+issues = fetch_repo_issues_graphql(
+    client,
+    owner="hiero-ledger",
+    repo="hiero-sdk-python",
+)
+
+print("Total issues:", len(issues))
